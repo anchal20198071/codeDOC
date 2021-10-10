5a8f0332f563a79de229e08d61c0dc6661d77a47
arya pandey <aryapandey002@gmail.com>
2021-10-10 18:51:17 +0530

    server feature added

---
+++ servercodeDoc/src/servercodedoc/Mysqlconnect.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servercodedoc;
+
+/**
+ *
+ * @author Arya Pandey
+ */
+public class Mysqlconnect {
+    
+}
---
+++ servercodeDoc/src/servercodedoc/ServercodeDoc.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servercodedoc;
+
+/**
+ *
+ * @author Arya Pandey
+ */
+public class ServercodeDoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
